717e5b96b87d5d573ab82b61e6ba636e60a58c10
Yige Li <yigesd@gmail.com>
2021-12-09 17:43:33 -0500

    File conversion completed

--- main.py
+++ main.py
@@ -2,5 +2,43 @@
 main file
 '''
 
-def main():
- pass
+
+def fileConversion():
+    #Format of the library files
+    # <book name>#<number of copies>#<restricted>
+
+    #blOpen -> Open the booklist.txt
+    blOpen = open("booklist.txt", "r")
+    #bookList -> Array of booklist
+    bookList = []
+
+    #llOpen -> Open the librarylog.txt
+    llOpen = open("librarylog-1.txt", "r")
+    #libraryLog -> Array of library log
+    libraryLog = []
+
+    s = blOpen.readline()
+    s = s.rstrip("\n")
+
+    while s != "":
+
+      a = s.split("#")
+      bookList.append(a)
+      s = blOpen.readline()
+      s = s.rstrip("\n")
+
+    s = llOpen.readline()
+    s = s.rstrip("\n")
+
+    while s != "":
+
+      a = s.split("#")
+      libraryLog.append(a)
+      s = llOpen.readline()
+      s = s.rstrip("\n")
+
+    print (bookList)
+    print (" ")
+    print (libraryLog)
+
+fileConversion()
